4aec2dcaff55063cd4e590695da06270696de580
sjx <2621280517@qq.com>
2023-09-20 13:19:41 +0800

    first.

---
+++ src/main/java/com/sjx/javablog/JavaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sjx.javablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBlogApplication.class, args);
+    }
+
+}
